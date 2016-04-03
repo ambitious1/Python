5cff4c32eb2810ead24b4ae71fdd0a59c6c0e2c1
Travis D <travis.dacosta1@gmail.com>
2016-04-03 11:22:22 -0400

    Added files via upload

---
+++ lambda.py
@@ -0,0 +1,17 @@
+def epic(a,b):
+    oneLineFunc = lambda a, b: a+b
+    print oneLineFunc(a, b)
+
+
+def main():
+    x = input("Enter a number: ")
+    y = input("Another number: ")
+    epic(x, y)
+
+
+if __name__ == '__main__':
+    main()
+    
+
+
+
