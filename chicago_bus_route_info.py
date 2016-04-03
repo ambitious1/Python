79c7aa121ff8c359f63ec6beeccd45690735b32e
Travis D <travis.dacosta1@gmail.com>
2016-04-03 02:08:19 -0400

    Update chicago_bus_route_info.py
    This uses Chicago public transportation website, and gives a live representation of when a specific bus on a specified route, arrives at each stop. The web page is analyzed as an XML document in which I extract the information I need, and write the data to an XML document. 
    Utilizing urllib.request module and file handling.

--- chicago_bus_route_info.py
+++ chicago_bus_route_info.py
@@ -6,3 +6,10 @@ f=open('rt22.xml','wb')
 f.write(data)
 f.close()
 input("Press <ENTER> to continue...")
+
+
+
+'''
+This uses Chicago public transportation website, and gives a live representation of when a specific bus on a specified route, arrives at each stop. The web page is analyzed as an XML document in which I extract the information I need, and write the data to an XML document. 
+Utilizing urllib.request module and file handling.
+'''
