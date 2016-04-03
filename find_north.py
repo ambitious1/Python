dfa1bc02d9bbacb25de409d21504e3d730c8bb9a
Travis D <travis.dacosta1@gmail.com>
2016-04-03 11:21:40 -0400

    Added files via upload

---
+++ find_north.py
@@ -0,0 +1,19 @@
+#find_north.py
+#
+#Find all buses that are traveling northbound of Dave's office
+
+dave_latitude = 41.98062
+dave_longtitude = -87.668452
+
+from xml.etree.ElementTree import parse
+doc = parse("rt22.xml")
+
+for bus in doc.findall("bus"):
+    lat = float(bus.findtext("lat"))
+    if lat > dave_latitude:
+        direction = bus.findtext('d')
+        if direction.startswith("North"):
+            busid = bus.findtext("id")
+            print busid, lat
+
+
